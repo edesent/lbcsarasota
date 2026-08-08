6457fa3c0bb1e72515c447a8d2f3ef2f4bf75939
aaiken12 <aaiken12@yahoo.com>
2026-08-07 21:27:14 -0400

    Filter sermon page to completed streams

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -24,7 +24,7 @@ export const dynamic = "force-dynamic";
 
 export default async function MessagesPage() {
   const [videos, live] = await Promise.all([
-    getRecentVideos(12),
+    getPastLivestreams(12),
     getLiveStatus(),
   ]);
 
